8ada1ac66c051aac2f59093b7a95bcff63599a1b
nekogato <05kcwong@gmail.com>
2022-03-30 08:36:00 +0800

    fix

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,3 @@
-import '../styles/global.css'
-
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+export default function handler(req, res) {
+  res.status(200).json({ text: 'Hello' })
 }
